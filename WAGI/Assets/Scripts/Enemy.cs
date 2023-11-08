a3bb1c8f83923319e7e0a06bac5c862f64435f83
ssohy <sohyeonj415@gmail.com>
2023-11-08 20:47:24 +0900

    merge: 정민파일업로드 및 에러해결

--- WAGI/Assets/Scripts/Enemy.cs
+++ WAGI/Assets/Scripts/Enemy.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+//using System.Numerics;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public string enemyName;
     public float speed;
     public int health;
     public Sprite[] sprites;
 
-    SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
 
-    void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = UnityEngine.Vector2.down * speed;
+    public float maxShotDelay;
+    public float curShotDelay;
 
-    }
+    public GameObject bulletObjectA;
+    public GameObject bulletObjectB;
+    public GameObject player;
+
+
+    SpriteRenderer spriteRenderer;
+
     void OnHit(int dmg)
     {
         health -= dmg;
         spriteRenderer.sprite = sprites[1];
         Invoke("ReturnSprite", 0.1f);
-        if (health <= 0){
+        if (health <= 0)
+        {
             Destroy(gameObject);
         }
     }
@@ -43,8 +48,56 @@ public class Enemy : MonoBehaviour
             Bullet bullet = collision.gameObject.GetComponent<Bullet>();
             OnHit(bullet.dmg);
 
-            
+
         }
     }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+    }
+
+    void Update()
+    {
+        Fire();
+        Reload();
+    }
+    void Fire()
+    {
+        if (curShotDelay < maxShotDelay)
+        {
+            return;
+        }
+        if (enemyName == "S")
+        {
+            GameObject bullet = Instantiate(bulletObjectA, transform.position, transform.rotation);
+            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+            Vector3 dirVec = player.transform.position - transform.position;
+            rigid.AddForce(dirVec * 10, ForceMode2D.Impulse);
+        }
+        else if (enemyName == "L")
+        {
+            GameObject bulletR = Instantiate(bulletObjectA, transform.position + Vector3.right * 0.3f, transform.rotation);
+            GameObject bulletL = Instantiate(bulletObjectA, transform.position + Vector3.left * 0.3f, transform.rotation);
+
+            Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+            Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+
+            Vector3 dirVecR = player.transform.position - (transform.position + Vector3.right * 0.3f);
+            Vector3 dirVecL = player.transform.position - (transform.position + Vector3.left * 0.3f);
+
+            rigidR.AddForce(dirVecR * 10, ForceMode2D.Impulse);
+            rigidL.AddForce(dirVecL * 10, ForceMode2D.Impulse);
+        }
+        curShotDelay = 0;
+    }
+    
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
 }
 
--- WAGI/Assets/Scripts/GameManger.cs
+++ WAGI/Assets/Scripts/GameManger.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     public float maxSpawnDelay;
     public float curSpawnDelay;
 
+    public GameObject player;
+
     void Update()
     {
         curSpawnDelay += Time.deltaTime;
@@ -25,10 +27,27 @@ public class GameManager : MonoBehaviour
     void SpawnEnemy()
     {
         int ranEnemy = Random.Range(0, 3);
-        int ranPoint = Random.Range(0, 5);
-        Instantiate(enemyObjs[ranEnemy],
+        int ranPoint = Random.Range(0, 9);
+        GameObject enemy=Instantiate(enemyObjs[ranEnemy],
                     spawnPoints[ranPoint].position,
                     spawnPoints[ranPoint].rotation);
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player=player;
+
+        if (ranPoint==5 || ranPoint == 6)
+        {
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed * (-1), -1);
+        }
+        else if (ranPoint == 7 || ranPoint == 8)
+        {
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else {
+            rigid.velocity = new Vector2(0, enemyLogic.speed * (-1));
+        }
 
     }
 }
